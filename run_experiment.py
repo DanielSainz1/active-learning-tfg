6aea4f9f03b642a365fe5002590b3699ed7672a2
DanielSainz1 <daniel.sainz@cunef.edu>
2026-04-10 13:41:10 +0200

    Print to test in colab

--- run_experiment.py
+++ run_experiment.py
@@ -11,12 +11,13 @@ from engine.trainer import train, evaluate
 from strategies.random_sampling import random_sampling
 from strategies.margin import margin_sampling
 from strategies.least_confidence import least_confidence
+from strategies.entropy import entropy
+from strategies.bald import bald
 from torchvision.transforms import ToTensor
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--strategy", default="random")
 args = parser.parse_args()
-
 strategy = args.strategy
 
 #Set up train data set
@@ -44,6 +45,14 @@ while len(labeled_indices) < CONFIG["budget"]:
     elif strategy == "least_confidence":
         positions = least_confidence(model, unlabeled_subset, CONFIG["query_batch_size"], device)
         selected = [unlabeled_indices[pos] for pos in positions]
+    elif strategy == "entropy":
+        positions = entropy(model, unlabeled_subset, CONFIG["query_batch_size"], device)
+        selected = [unlabeled_indices[pos] for pos in positions]
+    elif strategy == "bald":
+        positions = bald(model, unlabeled_subset, CONFIG["query_batch_size"], device, CONFIG["mc_forward_passes"])
+        selected = [unlabeled_indices[pos] for pos in positions]
+
+    print(f"Round | Labeled: {len(labeled_indices)}")
 
     labeled_indices.extend(selected)
     unlabeled_indices = [i for i in unlabeled_indices if i not in selected]
